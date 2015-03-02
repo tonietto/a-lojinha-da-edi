241ce6bf6016b9947ef663349e275df1f4ee2d97
Gustavo Tonietto <tonietto@gmail.com>
2015-03-02 17:22:51 -0300

    View incompleta
    Faltando instance para formset de quantidade por tamanho.

--- a_lojinha_da_edi/views.py
+++ a_lojinha_da_edi/views.py
@@ -1,16 +1,17 @@
 from django.forms.models import inlineformset_factory
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from .forms import PecaForm
 
 from catalogo.models import Peca, QuantidadeDePecasPorTamanho
 
 
-def admin_estoque_nova_peca(request):
+def admin_estoque_nova_peca(request, peca_id):
+    peca = Peca.objects.get(pk=peca_id)
     QuantidadeFormSet = inlineformset_factory(Peca, QuantidadeDePecasPorTamanho)
     if request.method == 'POST':
         form_principal = PecaForm(request.POST, request.FILES)
-        quantidade_formset = QuantidadeFormSet(request.POST, instance='Peca')
+        quantidade_formset = QuantidadeFormSet(request.POST, instance=peca)
 
         if form_principal.is_valid() and quantidade_formset().is_valid():
             form_principal.save(commit=True)
@@ -20,7 +21,7 @@ def admin_estoque_nova_peca(request):
         form_principal = PecaForm(prefix='peça')
         quantidade_formset = QuantidadeFormSet()
 
-    return render_to_response('admin_nova_peca.html', {
+    return render(request, 'admin_nova_peca.html', {
         'form_principal': form_principal,
         'quantidade_formset': quantidade_formset,
     })
